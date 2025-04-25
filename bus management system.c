9c7bae9f4c65d5a00989fa6e4f7f27e115f64684
saipatel005 <mandotisaikiran@gmail.com>
2025-04-25 21:40:18 +0530

    Update bus management system.c

--- bus management system.c
+++ bus management system.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-	int i,j,a[10][4],n,c=1,r=0;
+	int i,j,a[10][4],n,c=1,r=0,found=0;
 	char ch;
 	printf("SEATING ARRAGEMENT SYSTEM OF BUS\n");
 	for(i=0;i<10;i++)
@@ -18,22 +18,33 @@ int main()
 	{
 		printf("\nEnter seat number for registration: ");
 		scanf("%d",&n);
-		for(i=0;i<10;i++)
+		if(n>0 && n<=40)
 		{
-			printf("\n");
-			for(j=0;j<4;j++)
+			for(i=0;i<10;i++)
 			{
-				if(a[i][j]==n)
+				printf("\n");
+				for(j=0;j<4;j++)
 				{
-					a[i][j]=0;
-					r++;
+					if(a[i][j]==n)
+					{
+						a[i][j]=0;
+						r++;
+						found=1;
+					}
+					if(a[i][j]==0)
+					{
+						printf("XX\t");
+					}
+					else
+					printf("%d\t",a[i][j]);
 				}
-				printf("%d\t",a[i][j]);
 			}
+			printf("\nDo you want to continue registration: ");
+			fflush(stdin);
+			scanf(" %c",&ch);
 		}
-		printf("\nDo you want to continue registration: ");
-		fflush(stdin);
-		scanf("%c",&ch);
+		else
+		printf("The seat is already booked or not avaliable\n");
 	}
 	while(ch=='y' || ch=='Y');
 	printf("\n\nMaximum capacity of Bus is: 40\n");
